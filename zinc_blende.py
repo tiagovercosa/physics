529c51cd7b26a8a5c5fcf4418a2265eb87c518e4
Tiago Verçosa <vercosa@protonmail.com>
2021-10-18 21:34:06 -0300

    Lattice for zinc blende and 3D plotter

--- zinc_blende.py
+++ zinc_blende.py
@@ -1,11 +1,14 @@
 import kwant
 from matplotlib import pyplot
 
+# created lattice general
 lat = kwant.lattice.general([(0, 0.5, 0.5), (0.5, 0, 0.5), (0.5, 0.5, 0)],
                             [(0, 0, 0), (0.25, 0.25, 0.25)])
 a, b = lat.sublattices
 
+# make system at cubic form
 def make_system(t=1., a=5, b=50, c=5):
+    # created cube
     def make_cube(pos):
         x, y, z = pos
         return 0 <= x < a and 0 <= y < b and 0 <= z < c
@@ -15,15 +18,19 @@ def make_system(t=1., a=5, b=50, c=5):
     syst[lat.neighbors()] = t
     return syst
 
+# call the main function if the script get executed
 def main():
+    # The standard plotting for 3D.
     syst = make_system()
     kwant.plot(syst)
 
     syst = make_system(a=1.1, b=1.1, c=1.1)
 
+    # visualize the crystal structure better for a very small system.
     def family_colors(site):
         return 'brown' if site.family == a else 'c'
 
+#    the function has a bug:
 #    def family_size(site):
 #        return 0.15 if site.family == a else 0.1
 
